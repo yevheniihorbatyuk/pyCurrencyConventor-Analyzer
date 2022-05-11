4c680dec6ae9c9d64d97abd49399c6ad26982ea3
eugenesazak <60580192+eugenesazak@users.noreply.github.com>
2022-05-11 19:34:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from test_main import get_currency, currency_analysis
+
+# start_date = input('Start :')
+# end_date = input('End :')
+
+start_date = '2011-06-03'
+end_date = '2012-06-03'
+# get_currency(start_date=start_date, end_date=end_date)
+
+currency_analysis()
